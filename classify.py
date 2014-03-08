735c75eda31fe8d9d5c30720288b6af12ab30557
Lance Lee <akakaaykay@hotmail.com>
2014-03-08 12:56:13 +0800

    Having trouble with UnicodeDecodeError

--- classify.py
+++ classify.py
@@ -28,49 +28,50 @@ def main():
             label_train.append(temp)
     print len(label_train)        
 
-    # tweets_train = []
-    # app_train = [[] for i in xrange(3)]
-    # goo_train = [[] for i in xrange(3)]
-    # mic_train = [[] for i in xrange(3)]
-    # twi_train = [[] for i in xrange(3)]
-    # # open and extract texts from tweets_train.txt
-    # with open('data/tweets_train.txt') as file:
-    #     contents = file.readlines()
-    # print len(contents)
-    # for i in range(len(label_train)):
-    #     text = process_line(contents[i])
-    #     if label_train[0] == 'apple':
-    #         if label_train[1] == 'positive':
-    #             app_train[0].append(text)
-    #         elif label_train[1] == 'negative':
-    #             app_train[1].append(text)
-    #         elif label_train[1] == 'neutral':
-    #             app_train[2].append(text)    
-    #     elif label_train[0] == 'google':
-    #         if label_train[1] == 'positive':
-    #             goo_train[0].append(text)
-    #         elif label_train[1] == 'negative':
-    #             goo_train[1].append(text)
-    #         elif label_train[1] == 'neutral':
-    #             goo_train[2].append(text)              
-    #     elif label_train[0] == 'microsoft':
-    #         if label_train[1] == 'positive':
-    #             mic_train[0].append(text)
-    #         elif label_train[1] == 'negative':
-    #             mic_train[1].append(text)
-    #         elif label_train[1] == 'neutral':
-    #             mic_train[2].append(text)  
-    #     elif label_train[0] == 'twitter':
-    #         if label_train[1] == 'positive':
-    #             twi_train[0].append(text)
-    #         elif label_train[1] == 'negative':
-    #             twi_train[1].append(text)
-    #         elif label_train[1] == 'neutral':
-    #             twi_train[2].append(text)  
+    tweets_train = []
+    app_train = [[] for i in xrange(3)]
+    goo_train = [[] for i in xrange(3)]
+    mic_train = [[] for i in xrange(3)]
+    twi_train = [[] for i in xrange(3)]
+    # open and extract texts from tweets_train.txt
+    with open('data/tweets_train.txt') as file:
+        contents = file.readlines()
+    print len(contents)
+    for i in range(len(label_train)):
+        text = process_line(contents[i])
+        if label_train[0] == 'apple':
+            if label_train[1] == 'positive':
+                app_train[0].append(text)
+            elif label_train[1] == 'negative':
+                app_train[1].append(text)
+            elif label_train[1] == 'neutral':
+                app_train[2].append(text)    
+        elif label_train[0] == 'google':
+            if label_train[1] == 'positive':
+                goo_train[0].append(text)
+            elif label_train[1] == 'negative':
+                goo_train[1].append(text)
+            elif label_train[1] == 'neutral':
+                goo_train[2].append(text)              
+        elif label_train[0] == 'microsoft':
+            if label_train[1] == 'positive':
+                mic_train[0].append(text)
+            elif label_train[1] == 'negative':
+                mic_train[1].append(text)
+            elif label_train[1] == 'neutral':
+                mic_train[2].append(text)  
+        elif label_train[0] == 'twitter':
+            if label_train[1] == 'positive':
+                twi_train[0].append(text)
+            elif label_train[1] == 'negative':
+                twi_train[1].append(text)
+            elif label_train[1] == 'neutral':
+                twi_train[2].append(text)  
 
 def process_line(line):
     # json_dict = json.loads(line)
-    text = unicode(json.loads(line)['text'], errors='replace')
+    text =  json.loads(line)['text']
+    
     # bonus = grab_info(json_dict)
     # if "retweeted_status" in json_dict:
     #     bonus2 = grab_info(json_dict["retweeted_status"])
